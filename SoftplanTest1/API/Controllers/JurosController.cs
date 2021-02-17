a567f023fad60d26899cbca3d1257fc6e7d8de6b
Vinicius <vinicius.barbosa@rehagro.com.br>
2021-02-17 17:14:01 -0300

    add react app project 1

---
+++ SoftplanTest1/API/Controllers/JurosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JurosController : ControllerBase
+    {
+        public JurosController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(0.01);
+        }
+    }
+}
